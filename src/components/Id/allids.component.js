1ae5b6c5b54a4fe658f4655c7df3ef3d9bf31846
Machariajane <jacklinemasharia@gmail.com>
2022-01-06 12:52:46 +0300

    Added iLoading and isError functionalities to allids.componenet

--- src/components/Id/allids.component.js
+++ src/components/Id/allids.component.js
@@ -1,4 +1,4 @@
-// This component shows all ids,cards of 3x4 matrix starting with recents posted Ids
+// This component shows all ids,cards of nx3 matrix starting with recents posted Ids
 // has a search button,filter
 
 import React from "react";
@@ -11,13 +11,22 @@ import { Link } from 'react-router-dom';
 export const Allids = () =>{
   const classes=useStyles()
   const [data, setData] = useState({ data: [] });
+  const [isLoading,setIsLoading] = useState (false);
+  const [isError,setIsError] = useState(false);
  
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios('ids',
-      );
+      setIsError(false);
+      setIsLoading(true);
+
+      try{
+      const result = await axios('ids',);
  
       setData(result.data);
+      } catch(error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
     };
  
     fetchData();
@@ -25,9 +34,13 @@ export const Allids = () =>{
  
   return(
     <div>
+
+      {isError && <div>Something went wrong ...  </div>}
+      
+      {isLoading ? (<div>Loading ... </div>) : (
+    <main>
     <CssBaseline />
     
-    <main>
      
     <Container className={classes.cardGrid} maxWidth='md'>
       <Grid container spacing={4}>
@@ -54,6 +67,7 @@ export const Allids = () =>{
       </Grid>
     </Container>
     </main>
+    )}
     </div>
   )
 }
